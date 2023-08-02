6ef7de00102394fef16a4b52a3fa05883adc94f4
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2023-08-02 14:31:22 +0900

    [unrated] Title: 배열 조각하기, Time: 1.36 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -1,30 +1,7 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-       LinkedList<Integer> list = new LinkedList<Integer>();
-	for(int i:arr) list.add(i);
-	
-	for(int i=0; i<query.length;i++) {
-		if(i%2==0) {
-			int index = query[i]+1;
-			int len = list.size();
-			
-			for(int even=index; even<len;even++) {
-				list.pollLast();
-			}
-		}
-		if(i%2!=0) {
-			int index2 = query[i];
-			for(int odd=0; odd<index2; odd++) {
-				list.pollFirst();
-			}
-		}
-
-	}
-	int[] answer = new int[(list.size())];
-	for(int i=0; i<answer.length;i++) {
-		answer[i] = list.get(i);
-	}
+        int[] answer = {};
+        
         
         
         
