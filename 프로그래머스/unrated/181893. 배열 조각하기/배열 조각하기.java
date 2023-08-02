1f1f98f58c8cb9face66c70b155c1f8ced8f09ee
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2023-08-02 15:42:12 +0900

    [unrated] Title: 배열 조각하기, Time: 26.09 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -1,13 +1,29 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        int[] answer = {};
-        
-        
-        
-        
-        
-        
-        
+        int[] answer = arr;
+        int cnt =0;
+
+        for(int i = 0; i < query.length; i++) {
+        	//System.out.println(Arrays.toString(answer));
+        	if(i % 2 == 0) {
+        		int index = query[i]+1;
+        		int len = answer.length;
+        		//System.out.println(index);
+        		for(int even = index; even<len; even++) {
+        		answer = Arrays.copyOfRange(answer, 0, answer.length-1);
+        		//System.out.println("1" + Arrays.toString(answer));
+        		}
+        	}
+        	else {
+        		int index = query[i];
+        		//System.out.println(index);
+        		for(int odd =0; odd<index; odd++) {
+        		answer = Arrays.copyOfRange(answer, 1, answer.length);
+        		//System.out.println("2" + Arrays.toString(answer));
+        		}
+        	}
+        }
         return answer;
-    }
+	}
 }
