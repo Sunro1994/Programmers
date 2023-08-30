f4bd83c020e274e261cfb47cd5852ea22f99024b
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2023-08-30 12:13:50 +0900

    [unrated] Title: 둘만의 암호, Time: 1.01 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,37 +1,19 @@
-import java.util.*;
-
 class Solution {
-		
     public String solution(String s, String skip, int index) {
-		List<String> list = new ArrayList<String>();
-		String answer = "";
-		
-		for(int i=97; i<=122;i++) {
-			list.add((char)i+"");
-		}
-		
-		for(int i=0; i<skip.length();i++) {
-			String x = skip.charAt(i)+"";
-			if(list.contains(x)) {
-				list.remove(list.indexOf(x));
-			}
-		}
+        StringBuilder answer = new StringBuilder();
 
-		for(int i=0; i<s.length();i++) {
-			char target = s.charAt(i);
-			for(int j=0; j<index;j++) {
-				
-			while(!list.contains(target+"")) {
-				target = ++target;
-			}
-			target = ++target;
-                while(!list.contains(target+"")) {
-				target = ++target;
-			}
-			if(target>=123) target = (char)((int)target-26);
-			}
-			answer +=target;
-		}
-        return answer;
+        for (char letter : s.toCharArray()) {
+            char temp = letter;
+            int idx = 0;
+            while (idx < index) {
+                temp = temp == 'z' ? 'a' : (char) (temp + 1);
+                if (!skip.contains(String.valueOf(temp))) {
+                    idx += 1;
+                }
+            }
+            answer.append(temp);
+        }
+
+        return answer.toString();
     }
 }
