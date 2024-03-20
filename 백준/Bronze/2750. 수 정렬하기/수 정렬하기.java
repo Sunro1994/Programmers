867612e294253cbd56e1ba163a9d11a5187f6f96
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2024-03-20 22:26:20 +0900

    [Bronze II] Title: 수 정렬하기, Time: 160 ms, Memory: 13036 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,19 +1,39 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]) {
-        
-        Scanner sc =new Scanner(System.in);
-        int cnt = sc.nextInt();
-        int[] arr = new int[cnt];
-        
-        for(int i = 0; i<cnt;i++){
-            arr[i]= sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            arr[i] = x;
         }
-        Arrays.sort(arr);
-        
-        for(int i: arr) System.out.println(i);
-        
-        
+
+        for (int i = 0; i < N-1; i++) {
+            for (int x = i+1; x<N; x++) {
+                if(arr[i]>arr[x]) {
+                    swap(i,x,arr);
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+    private static void swap(int i, int x, int[] arr) {
+        int tmp =0;
+        tmp = arr[i];
+        arr[i] = arr[x];
+        arr[x] = tmp;
     }
 }
