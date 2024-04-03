1ab13ff056a818d88b615499f7e53b90fae2fe45
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2024-04-04 02:06:25 +0900

    [Silver II] Title: DFS와 BFS, Time: 292 ms, Memory: 19888 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int M;
+    static int V;
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]); // 정점 번호가 작은 순서대로 방문하기 위해 각 리스트 정렬
+        }
+
+        visited = new boolean[N + 1];
+        dfs(V);
+        System.out.println();
+
+        visited = new boolean[N + 1];
+        bfs(V);
+    }
+
+    private static void dfs(int v) {
+        visited[v] = true;
+        System.out.print(v + " ");
+        for (int next : graph[v]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        visited[start] = true;
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            System.out.print(v + " ");
+            for (int next : graph[v]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
