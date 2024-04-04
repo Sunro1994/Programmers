18ae9613605ceb30975e7cdd6d5cede900079f46
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2024-04-04 20:42:08 +0900

    [Silver I] Title: 회의실 배정, Time: 488 ms, Memory: 43468 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static ArrayList<int[]> list = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            list.add(new int[]{start, end});
+        }
+
+        list.sort(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return Integer.compare(o1[0], o2[0]);
+                }
+                return Integer.compare(o1[1], o2[1]);
+            }
+        });
+
+        int endTime = list.get(0)[1];
+        int cnt = 1;
+
+        for (int i = 1; i < list.size(); i++) {
+            int nextStartTime = list.get(i)[0];
+            if (nextStartTime >= endTime) {
+                endTime = list.get(i)[1];
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
