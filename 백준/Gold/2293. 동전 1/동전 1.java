245e8691c5558a9ec8f3cfaec7575d3598df2de1
Sunro1994 <132982907+Sunro1994@users.noreply.github.com>
2024-04-23 21:20:21 +0900

    [Gold V] Title: 동전 1, Time: 88 ms, Memory: 11656 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int k;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        int[] wallet = new int[n+1];
+        int[] arr = new int[k+1];
+
+        arr[0] =1;
+
+        for (int i = 1; i <=n; i++) {
+            wallet[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i <=n; i++) {
+            for (int j = wallet[i]; j <= k; j++) {
+                if(j>=wallet[i]){
+                    arr[j] = arr[j] + arr[j - wallet[i]];
+                }
+            }
+        }
+
+        System.out.println(arr[k]);
+
+
+
+
+
+    }
+}
